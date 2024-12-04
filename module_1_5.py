bc3b006d54f1f4fe6d012fa82ee986ed404f3694
Jokerneirosave <olgachuiyanova@gmail.com>
2024-12-04 15:04:07 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var=(78, "winter", 5.0, True)
+print(immutable_var)
+#immutable_var[1]=spring#неизменяемость-свойство кортежей
+immutable_list=[3, 7, "summer"]
+immutable_list[2]="autumn"
+print(immutable_list)
